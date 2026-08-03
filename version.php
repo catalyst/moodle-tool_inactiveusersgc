b253341bd064d9ed5fb9c0b861b386cfcdb7b1bc
Waleed Hassan <waleed.hassan@catalyst-eu.net>
2026-08-03 13:54:49 +0100

    WR #463673: Bump plugin version
    Bump version so it can be installed/upgraded on top of the
    previously installed 2026070200 release.

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_inactiveusersgc';
-$plugin->version   = 2025092903;
+$plugin->version   = 2026080300;
 $plugin->requires  = 2021051700;
 $plugin->release   = '1.0.0';
 $plugin->maturity  = MATURITY_BETA;
